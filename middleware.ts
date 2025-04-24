5150f31ced107395f73d10e2e4879a5f65c9364a
rafaelsantoslv <rafaeldsantos442@gmail.com>
2025-04-24 17:49:52 -0300

    refactor: adicionado variavel de ambiente para uso local

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,12 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
+  const isDevByPass = process.env.SKIP_AUTH === 'true';
+
+  if (isDevByPass) {
+    return NextResponse.next();
+  }
+
   const token = request.cookies.get("auth_token")?.value
   const isAuthRoute = request.nextUrl.pathname.startsWith("/login") || request.nextUrl.pathname.startsWith("/register")
   const isPublicRoute = request.nextUrl.pathname === "/"
